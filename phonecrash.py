db0c68a7667d3ed5d77e99bba45aac0b7fa0b247
Raghav <84623947+CodeBoy032@users.noreply.github.com>
2021-06-03 11:18:31 +0530

    Add files via upload

--- phonecrash.py
+++ phonecrash.py
@@ -1,26 +1,27 @@
+# I am setting try for finally.
+# you can add exceptions to handle errors in the code
+
 try:
+	# import phonenumbers module for geocoder and carrier
 	import phonenumbers
-	import webbrowser
+	# print welcome to user
 	print("Welcome to phonecrash")
+	# giving warning to user about the country code
 	print("Enter the phonenumber with country code")
+
+	# input of number for the output
 	number=input("Enter your number: ")
 
-	if number==ValueError:
-		print("Error")
-
+	#using geocoder we will find the country
 	from phonenumbers import geocoder
 	ch_number = phonenumbers.parse(number, "CH")
 	print(geocoder.description_for_number(ch_number, "en"))
 
+	# using carrier we will find the service card
 	from phonenumbers import carrier
 	service_number = phonenumbers.parse(number, "RO")
 	print(carrier.name_for_number(service_number, "en"))
 
-except ValueError as e:
-	print("enter a valid number!..")
-
-except SyntaxError as e:
-	print("Error")
-
+# printing thanks to user for using code.
 finally:
 	print("Thanks for using phonecrash")
